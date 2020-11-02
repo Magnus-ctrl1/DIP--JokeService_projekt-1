96cc7fb01fbf38cbb441f364dfdf7e709dd3a92f
Magnus Nielsen <nerdmarster2000@gmail.com>
2020-11-02 12:10:07 -0800

    Commit on marster

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    databaseURI: 'mongodb+srv://Magnus:nerdmarster250@cluster0.eat3v.mongodb.net/jokes?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
+    localPort: 8080
+};
+
+module.exports = config;
---
+++ public/index.js
@@ -0,0 +1,54 @@
+// index.js
+async function get(url) {
+    const respons = await fetch(url);
+    if (respons.status !== 200) // OK
+        throw new Error(respons.status);
+    return await respons.json();
+}
+
+async function getText(url) {
+    const respons = await fetch(url);
+    if (respons.status !== 200) // OK
+        throw new Error(respons.status);
+    return await respons.text();
+}
+
+async function generateJokesTable(jokes) {
+    let template = await getText('/index.hbs');
+    let compiledTemplate = Handlebars.compile(template);
+    return compiledTemplate({jokes});
+}
+
+async function main() {
+    try {
+        let jokes = await get('/joke/api/jokes');
+        let div = document.getElementById('div1')
+        div.innerHTML = await generateJokesTable(jokes);
+    } catch (e) {
+        console.log(e.name + ": " + e.message);
+    }
+}
+
+async function post(url, objekt) {
+    const respons = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(objekt),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    if (respons.status !== 201) // Created
+        throw new Error(respons.status);
+    return await respons.json();
+}
+
+
+
+main();
+let opretbutton = document.getElementById('opretbutton')
+opretbutton.onclick = async () => {
+    try {
+await post("/joke/api/jokes", {setup: "setup.value", punchline: "punchline.value"})
+
+    } catch(e){
+
+    }
+}
